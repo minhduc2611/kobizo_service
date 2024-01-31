90d469c98024d2d0251f3ef3ae7d977b24f8870d
minhducle <minhduc261298@gmail.com>
2024-01-31 09:46:07 +0700

    add icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,44 +3,29 @@ import logo from "./logo.svg";
 import close from "./close.svg";
 import menu from "./menu.svg";
 
+import discord from "./discord.svg";
+import facebook from "./facebook.svg";
 import iconBrain from "./iconBrain.png";
+import iconConsult from "./iconConsult.png";
 import iconGuide from "./iconGuide.png";
 import iconIdentity from "./iconIdentity.png";
-import iconConsult from "./iconConsult.png";
 import iconImplement from "./iconImplement.png";
+import infoBg from "./info-bg.jpeg";
+import instagram from "./instagram.svg";
+import linkedIn from "./linkedIn.svg";
 import noise from "./noise2.png";
 import space1 from "./space1.svg";
 import space2 from "./space2.svg";
 import space3 from "./space3.svg";
-import infoBg from "./info-bg.jpeg";
-import instagram from "./instagram.svg";
-import facebook from "./facebook.svg";
-import discord from "./discord.svg";
-import linkedIn from "./linkedIn.svg";
 import twitter from "./twitter.svg";
-import solar from "./solar.svg";
 
 import arrowRight from "./arrowRight.svg";
 
 export {
-  iconBrain,
-  iconGuide,
-  iconIdentity,
-  iconConsult,
-  logo,
-  arrowRight,
-  iconImplement,
-  close,
-  noise,
-  menu,
-  space1,
+  arrowRight, close, discord, facebook, iconBrain, iconConsult, iconGuide,
+  iconIdentity, iconImplement, infoBg,
+  instagram, linkedIn, logo, menu, noise, space1,
   space2,
-  space3,
-  infoBg,
-  instagram,
-  facebook,
-  discord,
-  linkedIn,
-  twitter,
-  solar
+  space3, twitter
 };
+
