5067088022d2d3e0023217d2b6f12408b7ce6d7b
minhducle <minhduc261298@gmail.com>
2024-02-01 14:55:00 +0700

    add mobile

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,10 +10,7 @@ const Button: FC<Props> = ({
 }: PropsWithChildren<Props>) => {
   return (
     <button
-      className={cn(
-        className,
-        "px-4 py-6 rounded-sm inline-block text-center text-white text-2xl font-semibold font-['Source Sans Pro']"
-      )}
+      className={cn(className, "px-4 py-6 rounded-sm inline-block text-center text-white text-2xl font-semibold font-['Source Sans Pro']")}
       {...props}
     >
       {children}
